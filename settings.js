975b3bbc3759f4d60965bf88143f201dedab5c05
Funixxxxxxxxx92 <wiwikdianpertiwi@gmail.com>
2024-09-29 15:08:15 +0700

    upload via termux

---
+++ settings.js
@@ -0,0 +1,11 @@
+// OBEY COMMANDMENTS
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['6285641124464'] //OWNER
+global.botname = 'Finix 444' //BOT NAME
+global.versisc = "4.4.4" //NOT CHANGE
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+global.creator = "Tama_Ultraa" //NOT CHANGE
+global.packname = "Allahumma SholliAla Sayyidina Muhammad Wa Ala Ali Sayyidina Muhammad" //OPSIONAL
+global.author = "Alhamdulillah 🥰♥️" //OPSIONAL
+global.simbol = "✇" // SIMBOL MENU
